68e540d0b35abb6c09defa209d898245a97dc1b0
Fatih-Tarim <fatihtrm99@gmail.com>
2023-04-03 16:12:32 +0300

    First Commit

---
+++ haberler/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Makale(models.Model):
+    yazar = models.CharField(max_length=150)
+    baslik = models.CharField(max_length=120)
+    aciklama = models.CharField(max_length=200)
+    metin = models.TextField()
+    sehir = models.CharField(max_length=120)
+    yayımlanma_tarihi = models.DateTimeField()
+    aktif = models.BooleanField()
+    yaratilma_tarihi=models.DateTimeField(auto_now_add=True)
+    güncellenme_tarihi = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.baslik
+    
