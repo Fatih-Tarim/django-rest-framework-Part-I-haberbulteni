d7b5e2d8d84efbca1a907f615ba7653e33412577
Fatih-Tarim <fatihtrm99@gmail.com>
2023-04-04 18:44:51 +0300

    makale detail view

--- haberler/api/views.py
+++ haberler/api/views.py
@@ -19,3 +19,35 @@ def makale_list_create_api_view(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+@api_view(['GET','PUT','DELETE'])
+def makale_detail_api_view(request, pk):
+    try:
+        makale_instance = Makale.objects.get(pk=pk)
+    except Makale.DoesNotExist:
+        return Response(
+            {
+                'errors':{
+                    'code':404,
+                    'message': f'böyle bir makale (id={pk}) bulamadım :('
+                }
+            },
+            status=status.HTTP_404_NOT_FOUND
+        )
+    
+    if request.method == 'GET':
+        serializer = MakaleSerializer(makale_instance) 
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = MakaleSerializer(makale_instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        makale_instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
